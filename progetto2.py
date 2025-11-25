aabac7aca6660f6a33dee4b40acc4df687fe07e0
Moussa <salisoumoussa1@gmail.com>
2025-11-25 17:14:19 +0100

    Creazione e gestione delle dictionary

---
+++ progetto2.py
@@ -0,0 +1,24 @@
+rubrica = {
+    100: {
+        "nome": "Anna",
+        "Cognome": "Barco",
+        "Classe": "4A"
+    },
+    101: {
+        "nome": "Marco",
+        "Cognome": "Marroni",
+        "Classe": "C5"
+    },
+    103: {
+        "nome": "Luca",
+        "Cognome": "Russo",
+        "Classe": "4b"
+    }
+}
+
+# Aggiungere un nuovo studente
+
+# Modificare la classe di uno studente specifico
+
+# Selezionare uno studente in base al Id (indice)
+# usando una condizione (se trova lo studente dice studente trovato se no studente non trovato)
