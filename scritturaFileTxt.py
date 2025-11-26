74a462b1b50ef21e73de0402d1b396d4c44b32ad
Moussa <salisoumoussa1@gmail.com>
2025-11-26 17:51:45 +0100

    Update project

---
+++ scritturaFileTxt.py
@@ -0,0 +1,5 @@
+# Creare e scrivere in un file di testo
+with open("appunti.txt", "w", encoding="utf-8") as f:
+    f.write("Questo è il mio primo file.\n")
+    f.write("Sto imparando a usare i file con Python!😉\n")
+    f.write("Ora so come usare Sql server e Python🎉")
