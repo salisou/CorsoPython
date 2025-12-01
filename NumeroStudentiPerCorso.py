a010c70cceece5d1996bad03cd52cb0214e4708c
Moussa <salisoumoussa1@gmail.com>
2025-12-01 18:01:36 +0100

    Update project Studente Crud

--- NumeroStudentiPerCorso.py
+++ NumeroStudentiPerCorso.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import matplotlib.pyplot as plt
 import pandas as pd
 import pyodbc as db
+from sqlalchemy import true
 
 DRIVER_NAME = "ODBC Driver 17 for SQL Server"
 SERVER_NAME = r"(localdb)\ServerPythonSql"
@@ -21,7 +22,9 @@ try:
 
     # Usa la store procedure
     query = "EXEC sp_NumeroStudentiPerCorso;"
+    query2 = "EXEC sp_MediaVotiPerStudente;"
     df = pd.read_sql(query, conn)
+    df2 = pd.read_sql(query2, conn)
     conn.close()
 
     print(df)
@@ -38,10 +41,23 @@ try:
     plt.show()
 
 
+
+
     # 2 Grafico a torta
     plt.figure(figsize=(8, 8))
     plt.pie(df['NumeroStudenti'], labels=df['NomeCorso'], autopct='%1.1f%%')
-    plt.title("Distribuzione studenti per corso")
+    plt.title("Distribuzione di studenti per corso")
+    plt.show()
+
+    # 3 Grafico
+    plt.figure(figsize=(10, 5))
+    plt.plot(df['NomeCorso'], df['NumeroStudenti'], marker='o')
+    plt.title('Numero di studenti per corso')
+    plt.xlabel('Nome Corso')
+    plt.ylabel('Numero studenti')
+    plt.xticks(rotation=40, ha='right')
+    plt.grid(True)
+    plt.tight_layout()
     plt.show()
 except Exception as e:
     messagebox.showerror("Error", e)
